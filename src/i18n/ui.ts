f9b68f0803d717960d1342411045099832952bfc
Manuel <dragonfire069@gmail.com>
2024-09-23 16:27:06 +0200

    feat: Añadir soporte multilingüe con selector de idioma y traducciones

---
+++ src/i18n/ui.ts
@@ -0,0 +1,27 @@
+export const languages = {
+    es: 'Español',
+    en: 'English',
+};
+
+export const defaultLang = 'es';
+
+export const showDefaultLang = false;
+
+export const ui = {
+    es: {
+        'intro.titulo': 'Soy Aitor Lopez',
+        'intro.subtitulo': 'Ingeniero Aereoespacial Junior',
+        'sobremi.titulo': 'Sobre mi',
+        'sobremi.descSpan': '¡Buenas! Soy Aitor,',
+        'sobremi.desc': 'Un estudiante de ingeniería aeroespacial al borde de graduarme y dispuesto por embarcarme en mi carrera profesional. Dispuesto a aplicar mis conocimientos aeroportuarios en desarrollo y gestión de proyectos. Aspiro a albergar conocimiento sobre el mundo, expandiendo continuamente mis herramientas como ingeniero aeroespacial. Acompañame en este recorrido! ',
+        'proyectos.titulo': 'Proyectos',
+    },
+    en: {
+        'intro.titulo': 'I am Aitor Lopez',
+        'intro.subtitulo': 'Junior Aerospace Engineer',
+        'sobremi.titulo': 'About me',
+        'sobremi.descSpan': 'Hello! I am Aitor,',
+        'sobremi.desc': 'An aerospace engineering student on the verge of graduating and ready to embark on my professional career. Willing to apply my aeronautical knowledge in project development and management. I aspire to harbor knowledge about the world, continually expanding my tools as an aerospace engineer. Join me on this journey!',
+        'proyectos.titulo': 'Projects',
+    },
+} as const;
